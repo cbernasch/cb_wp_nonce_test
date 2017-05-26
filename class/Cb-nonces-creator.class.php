65b7c4b455d5dcf8aec20bc3be114821e54040ab
CBernasch <cbernasch@gmx.de>
2017-05-26 17:00:52 +0200

    basic creator update

--- class/Cb-nonces-creator.class.php
+++ class/Cb-nonces-creator.class.php
@@ -7,7 +7,59 @@ namespace cbernasch\cb_wp_nonce_test\creator;
 
 class CB_WP_Nonces_Creator{
 
+    private $url;
+    private $action;
 
+    /**
+     * @param null $url
+     * @param string $nonce_name
+     * @param $action
+     */
+    public function __construct($url = null, $action = -1, $nonce_name = '_wpnonce'){
+        $this->url = $url;
+        $this->action = $action;
+        $this->nonce_name = $nonce_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_url(){
+
+        return $this->url;
+    }
+
+    /**
+     * @param $url
+     */
+    public function set_url($url){
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_action(){
+
+        return $this->action;
+    }
+
+    /**
+     * @param $action
+     */
+    public function set_action($action){
+        $this->action = $action;
+    }
+
+    public function get_nonce_url(){
+        //TODO cb validation url
+        if(!$this->url){
+
+            return false;
+        }
+
+        return wp_nonce_url($this->url, $this->action, $this->nonce_name);
+    }
 }
 
 
