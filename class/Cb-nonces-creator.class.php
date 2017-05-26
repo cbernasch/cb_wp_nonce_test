6022a5f2a5c08617a6ae3c41682b68df04884d82
CBernasch <cbernasch@gmx.de>
2017-05-26 16:29:24 +0200

    initial classes

---
+++ class/Cb-nonces-creator.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace cbernasch\cb_wp_nonce_test\creator;
+
+/**
+ * CB WP Nonces Creator Class
+ */
+
+class CB_WP_Nonces_Creator{
+
+
+}
+
+
+
---
+++ class/Cb-nonces-validator.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace cbernasch\cb_wp_nonce_test\validator;
+
+/**
+ * CB WP Nonces Validator Class
+ */
+
+class CB_WP_Nonces_Validator{
+
+
+}
+
+
+
