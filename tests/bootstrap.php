c853fca9509c121dd2c627ec212d66a14bad364d
CBernasch <cbernasch@gmx.de>
2017-05-29 16:50:16 +0200

    code style fixed required

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 require_once 'wordpress-tests-lib/includes/functions.php';
 
 function load_required_files() {
-    require 'src/class-cb-nonces-creator.class.php';
-    require 'src/class-cb-nonces-validator.class.php';
+    require 'src/class-cb-nonces-creator.php';
+    require 'src/class-cb-nonces-validator.php';
 }
 tests_add_filter( 'muplugins_loaded', 'load_required_files' );
 
